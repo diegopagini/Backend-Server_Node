9a966c1365b09e63238f4595940ef2c9c981760c
Diego Pagini <dq@qbkconsulting.com>
2021-02-02 16:41:40 -0300

    primera ruta

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ const express = require('express');
 //Crear el servidor Express
 const app = express();
 
+//Rutas
+app.get('/', (req, resp) => {
+	resp.json({
+		ok: true,
+		mgs: 'Hola Mundo',
+	});
+});
+
 app.listen(3000, () => {
 	console.log('Servidor corrioento en puerto', 3000);
 });
